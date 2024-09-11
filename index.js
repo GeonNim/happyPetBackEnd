543454b4f8235896bbbd4ee22d81c264c6d64661
geon <yy4338@gmail.com>
2024-09-11 16:04:34 +0900

    허용

--- index.js
+++ index.js
@@ -11,9 +11,18 @@ const bodyParser = require('body-parser');
 const PORT = '8080';
 const app = express();
 
+const allowedOrigins = ['http://localhost:3000', 'https://happypetfrontend.geonnim.com'];
+
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: function (origin, callback) {
+      // 요청이 허용된 도메인에서 왔는지 확인
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   })
 );
